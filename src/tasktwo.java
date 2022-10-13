29242fbc1dc38bcb55c70b53110e98350e97a16e
Stevenochka <redlemmon714@gmail.com>
2022-10-14 00:12:00 +0300

    tasktwo

---
+++ src/tasktwo.java
@@ -0,0 +1,8 @@
+public class tasktwo {
+    public static void main(String[] args){
+        System.out.println(triArea(6,3));
+    }
+    public static int triArea(int a,int b){
+        return (a*b)/2;
+    }
+}
