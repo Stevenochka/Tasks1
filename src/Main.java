f9c6ed25ed3f15f396254c0ee06f4c4399882133
Stevenochka <redlemmon714@gmail.com>
2022-10-14 00:01:51 +0300

    taskone

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(ost(9,3));
+    }
+
+    public static int ost(int a, int b){
+        return a % b;
+    }
+}
