31b777229831b44ea908bd366e7aae0411f36e4e
Stevenochka <redlemmon714@gmail.com>
2022-10-14 10:56:18 +0300

    tasksix

---
+++ src/tasksix.java
@@ -0,0 +1,9 @@
+public class tasksix {
+    public static void main(String[] args){
+        char x = '[';
+        System.out.println(ctoa(x));
+    }
+    public static int ctoa(char x){
+        return x;
+    }
+}
