159957b213ee3c8790ed12fc4a1c9fca525fa6e5
Stevenochka <redlemmon714@gmail.com>
2022-10-14 11:18:27 +0300

    taskeight

---
+++ src/taskeight.java
@@ -0,0 +1,9 @@
+public class taskeight {
+    public static void main(String[] args){
+        System.out.println(nextEdge(10,8));
+    }
+
+    public static int nextEdge(int a, int b){
+        return a+b - 1;
+    }
+}
